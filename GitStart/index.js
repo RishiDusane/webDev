74dd6b492216ec01e3f1cde1e7ac52c131ac2f36
RishiDusane <hdusane499@gmail.com>
2026-09-12 21:55:34 +0530

    new

--- GitStart/index.js
+++ GitStart/index.js
@@ -15,3 +15,11 @@ basketHeading.style.color = 'green';
 const thanks = document.getElementById('thanks');
 
 thanks.innerHTML = '<p>Please visit us again</p>'
+// Write your code below:
+const fruits = document.getElementsByClassName('fruit')
+
+fruits[2].style.backgroundColor = 'yellow';
+
+for (let i = 0; i < fruits.length; i++){
+    fruits[i].style.fontWeight = 'bold';
+}
