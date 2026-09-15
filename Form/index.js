2e5c0c0ef11c359dec0581bea5af3fe6772a09f0
RishiDusane <hdusane499@gmail.com>
2026-09-15 15:29:07 +0530

    form

--- Form/index.js
+++ Form/index.js
@@ -1,15 +1,43 @@
-// Write your code below 
+// Write your code below:
 function handleFormSubmit(event) {
     event.preventDefault();
 
-    const username = event.target.username.value;
-    const email = event.target.email.value;
-    const phone = event.target.phone.value;
+    const userDetails = {
+        username: event.target.username.value,
+        email: event.target.email.value,
+        phone: event.target.phone.value,
 
-    localStorage.setItem("username", username);
-    localStorage.setItem("email", email);
-    localStorage.setItem("phone", phone);
+    };
+    localStorage.setItem(userDetails.email,
+        JSON.stringify(userDetails));
 
+    showUserOnScreeen(userDetails);
+
+    if (typeof event.target.reset === "function") {
+        event.target.reset();
+    }
 }
+
+function showUserOnScreeen(user) {
+    const parentElem = document.getElementById("listOfUsers");
+    const chileElem = document.createElement("li");
+
+    chileElem.textContent = `${user.username}, ${user.email}, ${user.phone}`;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "delete-btn";
+
+    deleteButton.onclick = () => {
+        localStorage.removeItem(user.email);
+
+        parentElem.removeChild(chileElem);
+
+    };
+
+    chileElem.appendChild(deleteButton);
+
+    parentElem.appendChild(chileElem);
+};
 
 module.exports = handleFormSubmit;
